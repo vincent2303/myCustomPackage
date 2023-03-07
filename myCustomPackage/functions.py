8d5a5bf3244d0d42c0c0c265e5c0bbd1baf60132
vincent <vincent.martin23pub@gmail.com>
2023-03-07 16:23:14 +0100

    second commit

--- myCustomPackage/functions.py
+++ myCustomPackage/functions.py
@@ -1,2 +1,6 @@
 def say_foo():
     print('Foo')
+
+
+def say_bar():
+    print('Bar')
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='myCustomPackage',
-    version='0.0.1',
+    version='0.0.2',
     author='Vincent2303',
     description='Testing installation of Package',
     long_description_content_type="text/markdown",
