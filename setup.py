12b3a13b6832d9e403bfcc299e8a4d958e9364de
vincent <vincent.martin23pub@gmail.com>
2023-03-07 16:16:49 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='myCustomPackage',
+    version='0.0.1',
+    author='Vincent2303',
+    description='Testing installation of Package',
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=['myCustomPackage'],
+    install_requires=[],
+)
